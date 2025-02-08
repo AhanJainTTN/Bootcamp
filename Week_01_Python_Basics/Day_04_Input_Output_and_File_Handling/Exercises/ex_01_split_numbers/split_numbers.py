7aa8e50898e3a5d8dd07186c84f6d7f2fae8f60a
Ahan Jain (MBA) <ahanjain@gmail.com>
2025-02-09 01:14:48 +0530

    Refactored code into functions

--- Week_01_Python_Basics/Day_04_Input_Output_and_File_Handling/Exercises/ex_01_split_numbers/split_numbers.py
+++ Week_01_Python_Basics/Day_04_Input_Output_and_File_Handling/Exercises/ex_01_split_numbers/split_numbers.py
@@ -2,32 +2,44 @@
 You have a number.txt, with each line a real number. Write a code to split this file into 3 files as follows: even.txt -- contain all even numbers odd.txt -- all odd number float.txt -- all floating point number. Use with() clause for file handling
 """
 
-with open(
-    "Python Basics/Input-Output and File Handling/main-1/files/numbers.txt", "r"
-) as numbers_file, open(
-    "Python Basics/Input-Output and File Handling/main-1/files/even.txt", "w"
-) as even_file, open(
-    "Python Basics/Input-Output and File Handling/main-1/files/odd.txt", "w"
-) as odd_file, open(
-    "Python Basics/Input-Output and File Handling/main-1/files/float.txt", "w"
-) as float_file:
+import os
 
-    for line in numbers_file:
-        curr_line = line.rstrip("\n")
 
-        try:
-            num = float(curr_line)
-            if "." in curr_line:
-                float_file.write(curr_line + "\n")
-                print(f"Wrote {curr_line} to float.txt")
-            elif num % 2 == 0:
-                even_file.write(curr_line + "\n")
-                print(f"Wrote {curr_line} to even.txt")
-            elif num % 2 == 1:
-                odd_file.write(curr_line + "\n")
-                print(f"Wrote {curr_line} to odd.txt")
-        except ValueError:
-            print(f"{curr_line} is not a number")
+def number_splitter(input_file_path, output_dir=os.getcwd()):
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    with open(input_file_path, "r") as numbers_file, open(
+        os.path.join(output_dir, "even.txt"), "w"
+    ) as even_file, open(os.path.join(output_dir, "odd.txt"), "w") as odd_file, open(
+        os.path.join(output_dir, "float.txt"), "w"
+    ) as float_file:
+
+        for line in numbers_file:
+            curr_line = line.rstrip("\n")
+
+            try:
+                num = float(curr_line)
+                if "." in curr_line:
+                    float_file.write(curr_line + "\n")
+                    print(f"Wrote {curr_line} to float.txt")
+                elif num % 2 == 0:
+                    even_file.write(curr_line + "\n")
+                    print(f"Wrote {curr_line} to even.txt")
+                elif num % 2 == 1:
+                    odd_file.write(curr_line + "\n")
+                    print(f"Wrote {curr_line} to odd.txt")
+            except ValueError:
+                print(f"{curr_line} is not a number")
+
+
+if __name__ == "__main__":
+
+    input_file_path = "/Users/ahan/Documents/GitHub/Bootcamp/Week_01_Python_Basics/Day_04_Input_Output_and_File_Handling/Exercises/ex_01_split_numbers/files/numbers.txt"
+    output_dir = "/Users/ahan/Documents/GitHub/Bootcamp/Week_01_Python_Basics/Day_04_Input_Output_and_File_Handling/Exercises/ex_01_split_numbers/files"
+
+    number_splitter(input_file_path, output_dir)
+
 
 # Why not just check for '.' in string - this fails if we are reading something other than a numeric value - only works if our file consists of purely real numbers
 # Why not a simple odd/even check i.e. num % 2 == 1/0 - If a number like 0.0 or 1.0 is encountered, even though it is technically a float, it is written to odd/even file.
