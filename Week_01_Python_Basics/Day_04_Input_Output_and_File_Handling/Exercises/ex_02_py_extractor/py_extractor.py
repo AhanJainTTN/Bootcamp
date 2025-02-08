144e91e5811d016a48921f16528a57373e689dd5
Ahan Jain (MBA) <ahanjain@gmail.com>
2025-02-09 01:17:04 +0530

    Refactored and improved logic.
    Refactored code into functions and improved logic while maintaining functionality. Also added support for handling comments in python script.

--- Week_01_Python_Basics/Day_04_Input_Output_and_File_Handling/Exercises/ex_02_py_extractor/py_extractor.py
+++ Week_01_Python_Basics/Day_04_Input_Output_and_File_Handling/Exercises/ex_02_py_extractor/py_extractor.py
@@ -1,7 +1,8 @@
 """
-Write a code to read a "Python_script.py" as inpit file and extract following information to prepare a JSON * all package name which the input Python script use * all function name which the input Python script define * all class name which the input Python script define * all the variable name which the input Python script define example output: { "package": ["os", "itertools"], "funation": ["function1", "function2"], "class": ["classA", "classB"], "variable": ["num", "i", "j"] }
+Write a code to read a "Python_script.py" as input file and extract following information to prepare a JSON * all package name which the input Python script use * all function name which the input Python script define * all class name which the input Python script define * all the variable name which the input Python script define example output: { "package": ["os", "itertools"], "function": ["function1", "function2"], "class": ["classA", "classB"], "variable": ["num", "i", "j"] }
 """
 
+import os
 import json
 
 
@@ -10,108 +11,75 @@ def extract_package(line, extracted_data):
     filtered_string = ""
 
     if line.startswith("from "):
-        filtered_string = line[5:]
-        stop_index = filtered_string.find(" import ")
-        filtered_string = (
-            filtered_string[:stop_index:] if stop_index != -1 else filtered_string
-        )
-
+        filtered_string = line[5:].split(" import ")[0]
     elif line.startswith("import "):
         filtered_string = line[7:]
-
     else:
         return
-
     # handling aliases and commas
     for part in filtered_string.split(","):
-        package = part.split(" as ")[0]
-        if "." in package:
-            package = package.split(".")[0]
+        package = part.split(" as ")[0].split(".")[0]
         extracted_data["package"].add(package.strip())
 
 
 def extract_function(line, extracted_data):
-
-    filtered_string = ""
-
     if line.startswith("def "):
-        filtered_string = line[4:]
-        stop_index = filtered_string.find("(")
-        filtered_string = (
-            filtered_string[:stop_index:] if stop_index != -1 else filtered_string
-        )
+        filtered_string = line[4:].split("(")[0]
         extracted_data["function"].add(filtered_string.strip())
-
-    else:
-        return
 
 
 def extract_class(line, extracted_data):
-
-    filtered_string = ""
-
     if line.startswith("class "):
-        filtered_string = line[6:]
-        stop_index = filtered_string.find("(")
-        filtered_string = (
-            filtered_string[:stop_index:] if stop_index != -1 else filtered_string
-        )
-        stop_at = ("(", ":")
-
-        if filtered_string.find(stop_at[0]) != -1:
-            stop_at_char = stop_at[0]
-
-        else:
-            stop_at_char = stop_at[1]
-
-        stop_index = filtered_string.find(stop_at_char)
-        filtered_string = (
-            filtered_string[:stop_index:] if stop_index != -1 else filtered_string
-        )
+        filtered_string = line[6:].split("(")[0].split(":")[0]
         extracted_data["class"].add(filtered_string.strip())
 
 
 # only works with PEP8 suggested spacing for variable assignments
 # implement ast for more robust variable extraction
 def extract_variable(line, extracted_data):
+    index = line.find(" = ")
 
-    curr_string = " = "
-    curr_index = line.find(curr_string)
-
-    if curr_index != -1:
-        filtered_string = line[:curr_index:]
-
+    if index != -1:
+        filtered_string = line[:index:]
         # account for type annotations
         filtered_string = filtered_string.split(":")[0]
-
-        # handle aliases and commas
+        # handle commas
         for variable in filtered_string.split(","):
             extracted_data["variable"].add(variable.strip())
-    else:
-        return
 
 
-extracted_data = {
-    "package": set(),
-    "function": set(),
-    "class": set(),
-    "variable": set(),
-}
+def extract_metadata(input_file_path, output_dir=os.getcwd()):
 
-with open(
-    "Python Basics/Input-Output and File Handling/main-2/files/python_script.py", "r"
-) as script_file, open(
-    "Python Basics/Input-Output and File Handling/main-2/files/script_info.json", "w"
-) as script_info_json:
+    os.makedirs(output_dir, exist_ok=True)
 
-    for line in script_file:
-        line = line.strip()  # Remove indentation or leading blank spaces
-        extract_package(line, extracted_data)
-        extract_function(line, extracted_data)
-        extract_class(line, extracted_data)
-        extract_variable(line, extracted_data)
+    with open(input_file_path, "r") as script_file, open(
+        os.path.join(output_dir, "script_info.json"), "w"
+    ) as script_info_json:
 
-    for key in extracted_data:
-        extracted_data[key] = list(extracted_data[key])
+        extracted_data = {
+            "package": set(),
+            "function": set(),
+            "class": set(),
+            "variable": set(),
+        }
 
-    json.dump(extracted_data, script_info_json)
+        for line in script_file:
+            line = line.strip()  # Remove indentation or leading blank spaces
+            if not line.startswith("#"):  # ignore comments
+                extract_package(line, extracted_data)
+                extract_function(line, extracted_data)
+                extract_class(line, extracted_data)
+                extract_variable(line, extracted_data)
+
+        for key in extracted_data:
+            extracted_data[key] = list(extracted_data[key])
+
+        json.dump(extracted_data, script_info_json)
+
+
+if __name__ == "__main__":
+
+    input_file_path = "/Users/ahan/Documents/GitHub/Bootcamp/Week_01_Python_Basics/Day_04_Input_Output_and_File_Handling/Exercises/ex_02_py_extractor/files/python_script.py"
+    output_dir = "/Users/ahan/Documents/GitHub/Bootcamp/Week_01_Python_Basics/Day_04_Input_Output_and_File_Handling/Exercises/ex_02_py_extractor/files/"
+
+    extract_metadata(input_file_path, output_dir)
