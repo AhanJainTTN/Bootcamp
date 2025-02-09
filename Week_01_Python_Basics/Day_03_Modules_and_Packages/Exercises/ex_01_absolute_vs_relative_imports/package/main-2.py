84ccfe2d84c64ecfd6f34d8f3ea4ac1758e94d2b
Ahan Jain (MBA) <ahanjain@gmail.com>
2025-02-09 17:12:42 +0530

    Simplified import demo

--- Week_01_Python_Basics/Day_03_Modules_and_Packages/Exercises/ex_01_absolute_vs_relative_imports/package/main-2.py
+++
@@ -1,7 +0,0 @@
-from package.greetings_en import say_hello as say_hello_en_abs
-from package.subpackage.greetings_de import say_hello as say_hello_de_abs
-
-name = "Ahan"
-
-print(f"Hello in English: {say_hello_en_abs(name)}")
-print(f"Hello in Deutsch: {say_hello_de_abs(name)}")
