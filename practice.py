8777dc2fce3fd606e5c5ece9b1a0d34a2a8a1c29
Ahan Jain <ahan.jain@tothenew.com>
2025-02-13 20:57:49 +0530

    Added garbage collector and reference management snippets.

--- practice.py
+++ practice.py
@@ -155,3 +155,64 @@ def class_in_fun(fval):
 
 mval = 22
 class_in_fun(mval)
+
+"""
+Implement a singleton class `Database` that ensures only one instance of the class can be created. Testing reference deletions and garbage collector.
+"""
+
+import sys
+import gc
+
+
+class Database:
+    curr_obj = None
+
+    def __new__(cls):
+        if cls.curr_obj is None:
+            cls.curr_obj = super().__new__(cls)
+        return cls.curr_obj
+
+    def __init__(self):
+        self.db_connect = "<connection_string>"
+
+    @classmethod
+    def __delref__(cls):
+        cls.curr_obj = None
+
+
+db_1 = Database()
+db_2 = Database()
+
+# Printing memory addressed using id()
+print(f"Memory Address (db_1): {id(db_1)}")
+print(f"Memory Address (db_2): {id(db_2)}")
+print(f"Same Memory Address: {id(db_1) == id(db_2)}")
+
+# 4 because 2 are from db_1 and db_2, 1 from curr_obj and 1 from sys.getrefcount()
+print(sys.getrefcount(db_1))
+
+del db_1
+del db_2
+
+print(sys.getrefcount(Database.curr_obj))
+Database.__delref__()
+print(sys.getrefcount(Database.curr_obj))  # garbage value since object does not exist
+
+gc.collect()
+
+db_1 = Database()
+db_2 = Database()
+
+# Printing memory addressed using id()
+print(f"Memory Address (db_1): {id(db_1)}")
+print(f"Memory Address (db_2): {id(db_2)}")
+print(f"Same Memory Address: {id(db_1) == id(db_2)}")
+
+# print(sys.getrefcount(Database.curr_obj))
+# print(sys.getrefcount(db_1))
+# print(sys.getrefcount(db_2))
+
+# Printing memory addressed using id()
+# print(f"Memory Address (db_1): {id(db_1)}")
+# print(f"Memory Address (db_2): {id(db_2)}")
+# print(f"Same Memory Address: {id(db_1) == id(db_2)}")
