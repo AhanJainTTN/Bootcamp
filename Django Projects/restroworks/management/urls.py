7d14fcfa8fcf903543bd37d9247a1de82c7a8598
Ahan Jain (MBA) <ahanjain@gmail.com>
2025-02-27 22:54:40 +0530

    Added forms for better user input management.

--- Django Projects/restroworks/management/urls.py
+++ Django Projects/restroworks/management/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("get", views.get_all_customers, name="get_all_customers"),
-    path("add/customer", views.show_customer_form),
+    path("add/customer", views.add_customer, name="add_customer"),
+    path("success-page", views.success_page, name="success_page"),
+    path("add/menu-item", views.add_menu_item, name="add_menu_item"),
 ]
--- Django Projects/restroworks/management/views.py
+++ Django Projects/restroworks/management/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Q
 from management.models import Customer, Order, MenuItem
-from management.forms import CustomerForm
+from management.forms import CustomerForm, MenuItemForm
+from django.urls import reverse
 
 # To-Do
 # CRUD for Customers
@@ -13,26 +14,30 @@ from management.forms import CustomerForm
 # Delete a customer
 
 
-def show_customer_form(request):
-    return render(request, "index.html")
+def success_page(request):
+    return HttpResponse("Request successful.")
 
 
+# Source: https://docs.djangoproject.com/en/5.1/topics/forms/
 def add_customer(request):
     if request.method == "POST":
         # create a form instance and populate it with data from the request:
-        form = CustomerForm(request.POST)
+        customer_form = CustomerForm(request.POST)
         # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponse("Registration successful.")
-
+        if customer_form.is_valid():
+            # process the data in form.cleaned_data as required - why clean a form - Each field in a Form class is responsible not only for validating data, but also for “cleaning” it – normalizing it to a consistent format.
+            # https://stackoverflow.com/questions/53594745/what-is-the-use-of-cleaned-data-in-django
+            # https://docs.djangoproject.com/en/5.1/ref/forms/api/#django.forms.Form.cleaned_data
+            # redirect to a new URL
+            cleaned_data = customer_form.cleaned_data
+            print(cleaned_data)
+            # print(type(cleaned_data), cleaned_data) # <class 'dict'>
+            return redirect("success_page")  # redirect to common success page
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = CustomerForm()
-
-    return render(request, "index.html", {"form": form})
+        customer_form = CustomerForm()
+    # Why {"form": form} Instead of just form - when rendering a template using render(), the context (data sent to the template) must be a dictionary
+    return render(request, "customer_form.html", {"form": customer_form})
 
 
 def get_customer(request, name):
@@ -62,6 +67,25 @@ def get_all_customers(request):
     # )
 
     return response
+
+
+def add_menu_item(request):
+    if request.method == "POST":
+        menu_item_form = MenuItemForm(request.POST, request.FILES)
+        print("Files: ", request.FILES)
+        if menu_item_form.is_valid():
+            print("Valid Form.")
+            cleaned_data = menu_item_form.cleaned_data
+            print("Cleaned Data: ", cleaned_data)
+            menu_item_form.save()
+
+            return redirect("success_page")
+        else:
+            print("Errors: ", menu_item_form.errors)
+    else:
+        menu_item_form = MenuItemForm()
+
+    return render(request, "menu-item_form.html", {"form": menu_item_form})
 
 
 def update_customer(request):
