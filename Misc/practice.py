65c59dc7e4f55ec34c492c2e7098419db7444291
Ahan Jain <ahan.jain@tothenew.com>
2025-02-27 19:05:10 +0530

    Updated practice snippets.

--- Misc/practice.py
+++ Misc/practice.py
@@ -1014,6 +1014,7 @@ my_second_dict = {"C": 3, "D": 4}
 # print(type(**my_first_dict))
 my_merged_dict = {**my_first_dict, **my_second_dict}
 print(my_merged_dict)
+# print(my_first_dict.update(my_second_dict))
 
 # Remember that the * operator works on any iterable object. It can also be used to unpack a string
 a = [*"RealPython"]
