7a2830187e2211e970674fe7e240aab88fbb5a17
Ahan Jain (PC) <ahanjain@gmail.com>
2025-03-02 20:33:21 +0530

    Created accounts app to manage logins

---
+++ Django Projects/restroworks/accounts/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return JsonResponse({"message": "Login successful"}, status=200)
+
+        return JsonResponse({"error": "Invalid credentials"}, status=400)
