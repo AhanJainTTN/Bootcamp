819fb1a5d31f259a25f6e7425707512fa4379bb0
Ahan Jain (PC) <ahanjain@gmail.com>
2025-03-02 20:34:32 +0530

    Split views and models into separate apps.

--- Django Projects/restroworks/management/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Order, MenuItem
-
-
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(MenuItem)
--- Django Projects/restroworks/management/urls.py
+++ Django Projects/restroworks/management/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("get", views.get_all_customers, name="get_all_customers"),
-    path("add/customer", views.add_customer, name="add_customer"),
+    # path("get", views.get_all_customers, name="get_all_customers"),
+    # path("add/customer", views.add_customer, name="add_customer"),
     path("success-page", views.success_page, name="success_page"),
-    path("add/menu-item", views.add_menu_item, name="add_menu_item"),
+    # path("add/menu-item", views.add_menu_item, name="add_menu_item"),
 ]
