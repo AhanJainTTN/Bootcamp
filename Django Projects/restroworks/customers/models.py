10c6d22c26bc42be54f07cb262d97bbcd86a6d4b
Ahan Jain (MBA) <ahanjain@gmail.com>
2025-03-04 01:25:48 +0530

    Fixed string notations.

--- Django Projects/restroworks/customers/models.py
+++ Django Projects/restroworks/customers/models.py
@@ -8,4 +8,4 @@ class Customer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user__username}"
+        return f"{self.user.username}"
