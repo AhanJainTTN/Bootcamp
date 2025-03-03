4bac484aa4e430df6dc89ad677c7b8680bf97e44
Ahan Jain <ahan.jain@tothenew.com>
2025-03-03 17:21:54 +0530

    Added support for admin action for marking items delivered.

--- Django Projects/restroworks/orders/admin.py
+++ Django Projects/restroworks/orders/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from orders.models import Order
 
-admin.site.register(Order)
+
+@admin.action(description="Mark selected orders as Delivered (3)")
+def make_delivered(modeladmin, request, queryset):
+    queryset.update(status=3)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["customer", "status", "total_price", "created_at"]
+    actions = [make_delivered]
+
+
+admin.site.register(Order, OrderAdmin)
