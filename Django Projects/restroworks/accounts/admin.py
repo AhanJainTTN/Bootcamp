f7f02aecf0556328f21d91aec889eb9613f3cd3b
Ahan Jain <ahan.jain@tothenew.com>
2025-03-03 17:17:20 +0530

    Updated admin website header and title.

--- Django Projects/restroworks/accounts/admin.py
+++ Django Projects/restroworks/accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_header = "Restroworks"
+admin.site.site_title = "Restroworks Admin Portal"
+admin.site.index_title = "Welcome to Restroworks Admin Portal"
