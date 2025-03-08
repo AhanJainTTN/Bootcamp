c992ffd7a677e8a648482330cf89bee33e2d85fa
Ahan Jain <ahan.jain@tothenew.com>
2025-03-08 16:28:07 +0530

    Started working on support for landing page redirects

--- Django Projects/restroworks/accounts/views.py
+++ Django Projects/restroworks/accounts/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from .forms import CustomerForm, CustomerAuthenticationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 
 def user_signup(request):
@@ -11,6 +13,7 @@ def user_signup(request):
             customer = form.save()
             if customer:
                 form = CustomerForm()
+
                 # return JsonResponse(
                 #     {
                 #         "message": "Customer registered successfully",
@@ -27,7 +30,16 @@ def user_login(request):
     if request.method == "POST":
         form = CustomerAuthenticationForm(data=request.POST)
         if form.is_valid():
+            # print(form)
+            login(request, form.get_user())
+            # redirect(reverse("menu:render_grid"))
             form = CustomerAuthenticationForm()
+            # print("Redirecting...")
+            return redirect(reverse("menu:render_grid"))
             # return JsonResponse({"message": "Login successful."}, status=200)
 
     return render(request, "login.html", {"form_data": form})
+
+
+def render_home(request):
+    return render(request, "home.html")
