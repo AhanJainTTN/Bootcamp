89563393f7280df5995ea690085ded99b216813d
Ahan Jain <ahan.jain@tothenew.com>
2025-03-08 16:27:38 +0530

    Started work on landing page

--- Django Projects/restroworks/accounts/urls.py
+++ Django Projects/restroworks/accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("signup/", views.user_signup, name="user_signup"),
     path("login/", views.user_login, name="user_login"),
+    path("home/", views.render_home, name="render_home"),
 ]
