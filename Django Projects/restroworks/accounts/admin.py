d245ace8613ca6a7acbf45175668baafacd4b3c3
Ahan Jain (PC) <ahanjain@gmail.com>
2025-03-09 14:18:30 +0530

    Customised admin templates

--- Django Projects/restroworks/accounts/admin.py
+++ Django Projects/restroworks/accounts/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-admin.site.site_header = "Restroworks"
-admin.site.site_title = "Restroworks Admin Portal"
 admin.site.index_title = "Welcome to Restroworks Admin Portal"
