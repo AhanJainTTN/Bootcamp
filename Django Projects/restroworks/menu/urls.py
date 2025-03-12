c10467900c19d77cc9ba274b899522f319072272
Ahan Jain <ahan.jain@tothenew.com>
2025-03-12 20:04:47 +0530

    view/all handles both user and staff requests

--- Django Projects/restroworks/menu/urls.py
+++ Django Projects/restroworks/menu/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path("view/grid/", views.render_grid, name="render_grid"),
+    path("view/all/", views.render_grid, name="render_grid"),
     # path("create/", views.create_item, name="create_item"),
     # path(
     #     "create/", login_required(views.MenuItemFormView.as_view()), name="create_item"
@@ -20,11 +20,11 @@ urlpatterns = [
         name="retrieve_item",
     ),
     # path("view/all", views.list_items, name="list_items"),
-    path(
-        "view/all/",
-        views.MenuItemListView.as_view(),
-        name="list_items",
-    ),
+    # path(
+    #     "view/all/",
+    #     views.MenuItemListView.as_view(),
+    #     name="list_items",
+    # ),
     path("update/<int:item_id>", views.update_item, name="update_item"),
     path("delete/<int:item_id>", views.delete_item, name="delete_item"),
 ]
