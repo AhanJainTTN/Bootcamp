d6fa91eb2d590785c80e5af66d58a116810f7a54
Ahan Jain <ahan.jain@tothenew.com>
2025-03-12 20:03:36 +0530

    Created login view for auth testing

--- Django Projects/testdrf/employees/views.py
+++ Django Projects/testdrf/employees/views.py
@@ -2,9 +2,30 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from .models import Employees
 from .serializers import EmployeeSerializer
+from rest_framework.response import Response
+from rest_framework.request import Request
+from django.contrib.auth import login, authenticate
+from rest_framework.decorators import api_view
+
+
+@api_view(["POST"])
+def user_login(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return Response({"message": "Login successful."}, status=200)
+
+        return Response({"error": "No user found."})
 
 
 class EmployeeCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
 
