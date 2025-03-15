a7d1da7aa444b29aade4ef6feee805196a7efc62
Ahan Jain <ahan.jain@tothenew.com>
2025-03-15 14:03:31 +0530

    Added authentication requirement to all views and moved login to customauth app.

--- Django Projects/testdrf/employees/views.py
+++ Django Projects/testdrf/employees/views.py
@@ -2,35 +2,37 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from .models import Employees
 from .serializers import EmployeeSerializer
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.request import Request
-from django.contrib.auth import login, authenticate
 from rest_framework.decorators import api_view
 
 
-@api_view(["POST"])
-def user_login(request):
-
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-
-        user = authenticate(request, username=username, password=password)
-
-        if user:
-            login(request, user)
-            return Response({"message": "Login successful."}, status=200)
-
-        return Response({"error": "No user found."})
+class CustomCBVEmployeeCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Employees.objects.all()
+    serializer_class = EmployeeSerializer
 
 
-class EmployeeCreateView(generics.CreateAPIView):
+class CustomCBVEmployeeDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Employees.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class CustomCBVAllEmployeesListView(APIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Employees.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class CustomCBVEmployeeCreateView(APIView):
     permission_classes = [IsAuthenticated]
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
 
 
 class EmployeeDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
 
@@ -42,11 +44,13 @@ class AllEmployeesListView(generics.ListAPIView):
 
 
 class EmployeeUpdateView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
     partial = True
 
 
 class EmployeeDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
