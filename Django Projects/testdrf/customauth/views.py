87cc7ab06939dcc6cff2048e4cf45a5628cb6b2a
Ahan Jain <ahan.jain@tothenew.com>
2025-03-15 14:02:45 +0530

    Updated login routes.

--- Django Projects/testdrf/customauth/views.py
+++ Django Projects/testdrf/customauth/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from django.contrib.auth import login, authenticate
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(["POST"])
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return Response({"message": "Login successful."}, status=200)
+
+        return Response({"error": "No user found."})
