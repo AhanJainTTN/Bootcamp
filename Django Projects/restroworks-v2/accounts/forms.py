7f365c9b641861ee9c48530f499929f56760d1ee
Ahan Jain <ahan.jain@tothenew.com>
2025-03-28 15:02:14 +0530

    Added clarifying comments for inherited form fields and moved all styling to __init__.

--- Django Projects/restroworks-v2/accounts/forms.py
+++ Django Projects/restroworks-v2/accounts/forms.py
@@ -10,7 +10,7 @@ from .validators import validate_file_extension
 # Answer (Partial): Best to do both - clean_field makes it easier to send error messages to the user but is not entirely foolproof as there is a possibility that in the duration the query is being run, an entry with that email is inserted into the database. This will pass the uniqueness check but cause data inconsistency in the database. Part of the point of database-level constraints is to be atomic – to check the constraint and perform the insert at the same time, so no-one else can interfere in the middle.
 
 
-# Only username, password1 and password2 fields are inherited - even though BaseUserCreation form's Meta class uses User model, it only defines fields = ("username", ) so username is inherited. Additionally, it also defines password1 and password2 explicitly so in total 3 fields are inherited.
+# Only password1 and password2 fields are inherited - even though BaseUserCreation form's Meta class uses User model, it only defines fields = ("username", ) inside the Meta class so username is not inherited. It only defines password1 and password2 explicitly so in total 2 fields are inherited. This is why excluding username but keeping it in __init__()
 # ModelForm is a regular Form which can automatically generate certain fields. The fields that are automatically generated depend on the content of the Meta class and, on which fields have already been defined declaratively. Basically, ModelForm will only generate fields that are missing from the form, or in other words, fields that weren’t defined declaratively.
 # no need to explicitly hash passwords - already taken care of
 class CustomerForm(UserCreationForm):
@@ -22,11 +22,7 @@ class CustomerForm(UserCreationForm):
         ),
     )
 
-    # TODO: Check order/flow of execution using Meta classes
-    # fields or exclude must always be defined if using a meta class
-    # fields from fields = [...] and explicilty defined fields are displayed
-    # exclude takes precedence over fields
-    # here excluding password1 and password2 has no effect as the inherited and explicity declared fields   override their inclusion or exclusion in the Meta class. Excluding username raises a KeyError exception because we are accessing that field in __init__. username exists in User so it's existence is controlled by the fields attribute of the meta class.
+    # Meta class has no control over non model fields - need to be styled in __init__()
     class Meta:
         model = User
         fields = [
@@ -34,44 +30,32 @@ class CustomerForm(UserCreationForm):
             "first_name",
             "last_name",
             "email",
-            # "password1",
-            # "password2",
         ]
-        # exclude only has control over the User fields - not the inherited or explicitly declared fields
-        # https://code.djangoproject.com/ticket/8620#no2
-        exclude = ["phone", "password1", "password2"]  # no effect
-
-        widgets = {
-            "first_name": forms.TextInput(
-                attrs={"class": "input-field", "placeholder": "Enter first name"}
-            ),
-            "last_name": forms.TextInput(
-                attrs={"class": "input-field", "placeholder": "Enter last name"}
-            ),
-            "email": forms.EmailInput(
-                attrs={"class": "input-field", "placeholder": "Enter email"}
-            ),
-        }
 
     # better way to add attributes as it does not override original InputFieldType
+    # field must be present in Meta class fields or inherited or defined explicitly
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["username"].widget.attrs.update(
             {"class": "input-field", "placeholder": "Enter username"}
         )
+        self.fields["first_name"].widget.attrs.update(
+            {"class": "input-field", "placeholder": "Enter username"}
+        )
+        self.fields["last_name"].widget.attrs.update(
+            {"class": "input-field", "placeholder": "Enter username"}
+        )
+        self.fields["email"].widget.attrs.update(
+            {"class": "input-field", "placeholder": "Enter username"}
+        )
+
+        # inherited fields
         self.fields["password1"].widget.attrs.update(
             {"class": "input-field", "placeholder": "Enter password"}
         )
         self.fields["password2"].widget.attrs.update(
             {"class": "input-field", "placeholder": "Enter password"}
         )
-
-    # def clean_email(self):
-    #     email = self.cleaned_data["email"]
-    #     if User.objects.filter(email=email).exists():
-    #         raise ValidationError(message="Email Already In Use.")
-
-    #     return email
 
     def save(self):
         try:
@@ -85,60 +69,8 @@ class CustomerForm(UserCreationForm):
             return customer
         # TODO: Find a better way. How much validation? HTML -> Forms -> Models. Which is the best practice?
         except IntegrityError as e:
-            # print(str(e.__cause__))
             cause = str(e.__cause__).split(".")[1]
             self.add_error(f"{cause}", "Already in use. Please enter a unique value.")
-
-
-# class CustomerForm(forms.ModelForm):
-#     phone = forms.CharField(
-#         max_length=10,
-#         required=True,
-#         widget=forms.TextInput(
-#             attrs={"class": "input-field", "placeholder": "Enter phone"}
-#         ),
-#     )
-
-#     class Meta:
-#         model = User
-#         fields = ["username", "first_name", "last_name", "email", "password"]
-# widgets = {
-#     "username": forms.TextInput(
-#         attrs={"class": "input-field", "placeholder": "Enter username"}
-#     ),
-#     "password": forms.PasswordInput(
-#         attrs={"class": "input-field", "placeholder": "Enter email"}
-#     ),
-#     "first_name": forms.TextInput(
-#         attrs={"class": "input-field", "placeholder": "Enter first name"}
-#     ),
-#     "last_name": forms.TextInput(
-#         attrs={"class": "input-field", "placeholder": "Enter last name"}
-#     ),
-#     "email": forms.EmailInput(
-#         attrs={"class": "input-field", "placeholder": "Enter email"}
-#     ),
-# }
-
-#     # Django's User model requires passwords to be hashed before saving.
-#     # Using fields = ["password"] without using PasswordField/PasswordInput will create a normal text field, which is insecure. Even if we just set password attributes, we still need to hash it.
-#     #  We also need password to be rendered as hidden field so we explicitly define it as a password field instead of just appending it to fields and hashing it using save.
-# def save(self):
-#     # If commit=False is used, Django creates an instance of the model but does NOT save it to the database
-#     # commit=False because still need to hash password and link User to Customer
-#     # super().save(commit=False)creates a User instance but does not save it.
-#     try:
-#         with transaction.atomic():
-#             user = super().save(commit=False)
-#             user.set_password(self.cleaned_data["password"])
-#             user.save()
-#             user = Customer.objects.create(
-#                 user=user, phone=self.cleaned_data["phone"]
-#             )
-
-#         return user
-#     except IntegrityError:
-#         self.add_error("phone", "This phone number is already in use.")
 
 
 class CustomerAuthenticationForm(AuthenticationForm):
@@ -150,26 +82,6 @@ class CustomerAuthenticationForm(AuthenticationForm):
         self.fields["password"].widget.attrs.update(
             {"class": "input-field", "placeholder": "Enter password"}
         )
-
-    # username
-    # password = forms.CharField(
-    #     widget=forms.PasswordInput(
-    #         attrs={"class": "input-field", "placeholder": "Enter password"}
-    #     ),
-    #     required=True,
-    # )
-
-    # class Meta:
-    #     model = User
-    #     fields = ["username", "password"]
-    #     widgets = {
-    #         "username": forms.TextInput(
-    #             attrs={"class": "input-field", "placeholder": "Enter username"}
-    #         ),
-    #         "password": forms.PasswordInput(
-    #             attrs={"class": "input-field", "placeholder": "Enter email"}
-    #         ),
-    #     }
 
 
 class ExcelForm(forms.Form):
